81a56958ab3893ca97ea5799ddf607ea37f66e77
Tanner Witt <tannerwitt@Tanners-MacBook-Air.local>
2023-04-12 00:19:05 -0500

    added eclipse fies

---
+++ src/main/java/animeadventures/aascript/App.java
@@ -0,0 +1,13 @@
+package animeadventures.aascript;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
